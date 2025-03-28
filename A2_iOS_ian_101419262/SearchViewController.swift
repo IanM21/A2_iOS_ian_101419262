7b7b00718d69de147cf22114cd2995d357eca568
IanM21 <ianmcdonad2003@gmail.com>
2025-03-28 10:38:22 -0400

    Created SearchViewController

---
+++ A2_iOS_ian_101419262/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  A2_iOS_ian_101419262
+//
+//  Created by ian mcdonald on 2025-03-28.
+//
+
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+}
